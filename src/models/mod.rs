215e98b372007f3affbfbbf19796ef4b1dca281a
kana-rus <kanarus786@gmail.com>
2024-05-15 06:41:05 +0900

    use Payload validation

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -7,12 +7,22 @@ pub struct SigninResponse {
     pub token: JWTToken,
 }
 
-#[Payload(JSON/S)]
+#[Payload(JSON/S where Self::validate)]
 pub struct Todo {
     pub id:        String,
     pub content:   String,
     pub completed: bool,
     pub tags:      Vec<Tag>,
+}
+impl Todo {
+    const TAGS_LIMIT: usize = 5;
+
+    fn validate(&self) -> Result<(), String> {
+        (self.tags.len() <= Self::TAGS_LIMIT).then_some(())
+            .ok_or_else(|| format!("One todo can have at most five tags"))?;
+
+        Ok(())
+    }
 }
 
 #[Payload(JSON/S)]
